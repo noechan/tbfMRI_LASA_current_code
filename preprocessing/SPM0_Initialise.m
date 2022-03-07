869afd6fe09043753c16177bdeaa7926db14c58a
Noelia Martinez Molina <noelia.martinezmolina@gmail.com>
2022-03-07 17:00:22 +0100

    added copyright

---
+++ preprocessing/SPM0_Initialise.m
@@ -0,0 +1,42 @@
+function SPM0_Initialise(indir, outdir, names, varargin)
+%SPM0_INITIALISE
+
+%Copyright(c) 2022: Noelia Martínez-Molina (noelia.martinez@upf.edu)
+
+%--------------------------------------------------------------------------
+% Initialise inputs
+p = inputParser;
+p.addRequired('indir', @ischar)
+p.addRequired('outdir', @ischar)
+p.addRequired('names', @isstruct)
+
+% TODO : MATCH BIDS SPECIFICATION
+p.addParameter('datdir', 'rs', @ischar)
+p.addParameter('rawdir', 'rs_rawdata', @ischar)
+
+p.parse(indir, outdir, names, varargin{:})
+Arg = p.Results;
+
+
+%% Loop to copy raw_data to new folder per subject
+for sbji = 1:size(names, 1)
+    srcfile = dir(fullfile(indir, names(sbji).name, Arg.rawdir, 'f*.nii'));
+    sub_path = fullfile(outdir, names(sbji).name, Arg.datdir);
+    if ~isfolder(sub_path)
+        mkdir(sub_path)
+    end
+    for sf = 1:length(srcfile)
+        if ~exist(fullfile(sub_path, srcfile(sf).name), 'file')
+            copyfile(fullfile(srcfile(sf).folder, srcfile(sf).name), sub_path)
+        end
+    end
+    [srcfile.folder] = deal(sub_path);
+    names(sbji).sources = srcfile;
+end
+
+%----------------------------------------------------
+% Initialise SPM
+spm('Defaults','fMRI');
+spm_jobman('initcfg');
+
+end
