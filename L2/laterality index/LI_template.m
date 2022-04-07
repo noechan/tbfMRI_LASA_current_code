7530fe62ce2cfd3bec73f04cc892b471f4268d1a
Noelia Martinez Molina <noelia.martinezmolina@gmail.com>
2022-04-07 18:57:00 +0200

    renamed

---
+++ L2/laterality index/LI_template.m
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+
+
+
+matlabbatch{1}.spm.tools.LI_cfg.spmT = '<UNDEFINED>';
+matlabbatch{1}.spm.tools.LI_cfg.inmask.im8 = 1;
+matlabbatch{1}.spm.tools.LI_cfg.exmask.em1 = 1;
+matlabbatch{1}.spm.tools.LI_cfg.method.thr7 = 1;
+matlabbatch{1}.spm.tools.LI_cfg.pre = 0;
+matlabbatch{1}.spm.tools.LI_cfg.op = 4;
+matlabbatch{1}.spm.tools.LI_cfg.vc = 0;
+matlabbatch{1}.spm.tools.LI_cfg.ni = 1;
+matlabbatch{1}.spm.tools.LI_cfg.outfile = 'li.txt';
